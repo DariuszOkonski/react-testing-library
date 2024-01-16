cb899d92a6168d3efdde7fdd7e8fd60c2d2c1b85
Dariusz Okoński <darek200180@gmail.com>
2024-01-16 22:03:22 +0100

    add test

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,12 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
+test('render one row per user ver 1', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'mark', email: 'mark@mark.com' },
+  ];
+  render(<UserList users={users} />);
+
+  const janeRow = screen.getByRole('row', { name: /jane jane@jane\.com/i });
+  const markRow = screen.getByRole('row', { name: /mark mark@mark\.com/i });
+
+  expect(janeRow).toBeInTheDocument();
+  expect(markRow).toBeInTheDocument();
+});
+
+test('render one row per user ver 2', () => {
   const users = [
     { name: 'jane', email: 'jane@jane.com' },
     { name: 'sam', email: 'sam@sam.com' },
   ];
   render(<UserList users={users} />);
+  // screen.logTestingPlaygroundURL();
+
+  const rows = screen.getAllByRole('row');
+
+  expect(rows).toHaveLength(3);
+});
+
+test('render one row per user ver 3', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+  render(<UserList users={users} />);
+
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+  expect(rows).toHaveLength(2);
+});
+
+test('render one row per user ver 4', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+  const { container } = render(<UserList users={users} />);
+
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll('tbody tr');
+
+  expect(rows).toHaveLength(2);
 });
 
 test.skip('render the email and name of each user', () => {});
