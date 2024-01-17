5e5e98ae8e6afc3d733e5afdb7a9c0bdf953bca9
Dariusz Okoński <darek200180@gmail.com>
2024-01-17 21:22:38 +0100

    add test

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -53,4 +53,18 @@ test('render one row per user ver 4', () => {
   expect(rows).toHaveLength(2);
 });
 
-test.skip('render the email and name of each user', () => {});
+test('render the email and name of each user v1', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
