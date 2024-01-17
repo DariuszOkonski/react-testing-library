3edf454531633ac8039cf21dbc58f8f1c0113d6e
Dariusz Okoński <darek200180@gmail.com>
2024-01-17 21:36:59 +0100

    test app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can receive a new user and show it on a list', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  expect(true).toBe(true);
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    user.click(nameInput);
+    user.keyboard('jane');
+
+    user.click(emailInput);
+    user.keyboard('jane@jane.com');
+
+    user.click(button);
+  });
+
+  // screen.debug();
+  const name = screen.getByRole('cell', { name: 'jane' });
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
