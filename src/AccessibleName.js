f37004ad8b013d16cf49f1d6926dc29874cbc86c
Dariusz Okoński <darek200180@gmail.com>
2024-01-18 17:36:12 +0100

    add AccessibleName

---
+++ src/AccessibleName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AccessibleName() {
+  return (
+    <div>
+      <button>Submit</button>
+      <button>Cancel</button>
+    </div>
+  );
+}
+
+export default AccessibleName;
