5f894e0120a8ce48dc9915a0b8263b609145cca4
Dariusz Okoński <darek200180@gmail.com>
2024-01-30 16:44:35 +0100

    add colorList

--- src/ColorList.test.js
+++ src/ColorList.test.js
@@ -26,3 +26,26 @@ test('getBy, queryBy, findby whe they find 1 element', async () => {
   expect(screen.queryByRole('list')).toBeInTheDocument();
   expect(await screen.findByRole('list')).toBeInTheDocument();
 });
+
+test('getBy, queryBy, findBy when findinding > 1 elements', async () => {
+  render(<ColorList />);
+
+  expect(() => screen.getByRole('listitem')).toThrow();
+  expect(() => screen.queryByRole('listitem')).toThrow();
+
+  let errorThrown = false;
+  try {
+    await screen.findByRole('listitem');
+  } catch (err) {
+    errorThrown = true;
+  }
+  expect(errorThrown).toEqual(true);
+});
+
+test('getAll, queryAllBy, findAllBy', async () => {
+  render(<ColorList />);
+
+  expect(screen.getAllByRole('listitem')).toHaveLength(3);
+  expect(screen.queryAllByRole('listitem')).toHaveLength(3);
+  expect(await screen.findAllByRole('listitem')).toHaveLength(3);
+});
