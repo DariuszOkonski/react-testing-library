b5056fd6ae5ce7265677e76032a0325988da8188
Dariusz Okoński <darek200180@gmail.com>
2024-02-01 15:38:15 +0100

    add FormData

--- src/FormData.test.js
+++ src/FormData.test.js
@@ -1,6 +1,22 @@
 import { render, screen, within } from '@testing-library/react';
 import FormData from './FormData';
 
+function toContainRole(container, role, quantity = 1) {
+  const elements = within(container).queryAllByRole(role);
+
+  if (elements.length === quantity) {
+    return {
+      pass: true,
+    };
+  }
+
+  return {
+    pass: false,
+    message: () =>
+      `Expected to find ${quantity} ${role} elements. Found ${elements.length} instead`,
+  };
+}
+
 // screen.logTestingPlaygroundURL();
 test('the form displays two buttons v1', () => {
   render(<FormData />);
@@ -19,4 +35,5 @@ test('the form displays two buttons v2', () => {
   expect(buttons).toHaveLength(2);
 
   // expect(form).toContainRole('button', 2);
+  expect.extend({ toContainRole });
 });
